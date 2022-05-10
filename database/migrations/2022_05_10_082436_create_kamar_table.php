9c5503647cff5eb5845310f75c010ce18f3595cc
Yogapratama1109 <yoga110011@gmail.com>
2022-05-10 15:42:47 +0700

    new:migration

---
+++ database/migrations/2022_05_10_082436_create_kamar_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKamarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kamar', function (Blueprint $table) {
+            $table->id('kamar_id');
+            $table->string('tipe');
+            $table->string('name');
+            $table->string('harga');
+            $table->string('fasilitas');
+            $table->string('jumlah');
+            $table->integer('max');
+            $table->enum('status', [0, 1])->comment('available', 'booked')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kamar');
+    }
+}
